b0bb3b823fa78e39e0265fce7c5b65e8d14d3e5c
Andres Leveri <afleverim@creasistemas.com>
2020-02-19 14:11:53 -0500

    Init repo

---
+++ app/src/main/java/com/desarrollomovil/taller1_aleveri/MainActivity.java
@@ -0,0 +1,14 @@
+package com.desarrollomovil.taller1_aleveri;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
